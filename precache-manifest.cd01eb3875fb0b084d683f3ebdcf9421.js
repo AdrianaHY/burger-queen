1bd7da2f7a2b7985c8b607bd7d3f2ee8a812edcd
AdrianaH <lhernandez@sparefoot.com>
2019-12-03 15:53:09 -0600

    Updates

---
+++ precache-manifest.cd01eb3875fb0b084d683f3ebdcf9421.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deda9cbd94e372d3c5b269cf65395385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67077b092c03169d7fa6",
+    "url": "/static/css/main.aa189678.chunk.css"
+  },
+  {
+    "revision": "b20bfaab79942110d36a",
+    "url": "/static/js/2.cbe138a4.chunk.js"
+  },
+  {
+    "revision": "67077b092c03169d7fa6",
+    "url": "/static/js/main.cf8c0df7.chunk.js"
+  },
+  {
+    "revision": "3794d6710617f0c5c55d",
+    "url": "/static/js/runtime-main.68b1434e.js"
+  },
+  {
+    "revision": "6218d49cd83fddb8cfc054a5bb1032d9",
+    "url": "/static/media/breakfast.6218d49c.svg"
+  },
+  {
+    "revision": "8287c7b9af8275b146f0b09b2b0e92e3",
+    "url": "/static/media/burger-and-soda.8287c7b9.svg"
+  },
+  {
+    "revision": "17d2df51f4574d674df8b68a621cab48",
+    "url": "/static/media/fast-food.17d2df51.svg"
+  },
+  {
+    "revision": "c9a74ae22e5a45b5f7558a60d8319910",
+    "url": "/static/media/paper-bin.c9a74ae2.svg"
+  },
+  {
+    "revision": "c2bd9a978a69229f654d533d8b9f2f2d",
+    "url": "/static/media/restaurant.c2bd9a97.svg"
+  },
+  {
+    "revision": "c43edba9406b644e45b84b42c34d6eab",
+    "url": "/static/media/user.c43edba9.svg"
+  }
+]);
